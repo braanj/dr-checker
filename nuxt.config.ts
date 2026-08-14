b0d57297629e8ac28569a202cc66f793ddfa705c
Brahim Anjjar <anjar.bra@gmail.com>
2026-08-14 22:41:48 +0100

    feat: add GTM id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,30 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
 
+  app: {
+    head: {
+      script: [
+        {
+          textContent: `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.GTM_ID}');</script>
+<!-- End Google Tag Manager -->`,
+        },
+
+        {
+          textContent: `<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`,
+          tagPosition: "bodyOpen",
+        },
+      ],
+    },
+  },
+
   runtimeConfig: {
     // server-only (never exposed to client)
     supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
