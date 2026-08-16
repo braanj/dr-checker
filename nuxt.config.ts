39db151239453c4ef93006ce6fc9a9e4a83468d5
Brahim Anjjar <anjar.bra@gmail.com>
2026-08-16 13:22:42 +0100

    fix: use noscript for GTM on body

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+const GTM_ID = process.env.GTM_ID;
+
 export default defineNuxtConfig({
   compatibilityDate: "2026-01-01",
   devtools: { enabled: true },
@@ -12,13 +14,16 @@ export default defineNuxtConfig({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${process.env.GTM_ID}');
+})(window,document,'script','dataLayer','${GTM_ID}');
 <!-- End Google Tag Manager -->`,
+          tagPriority: "critical",
+          type: "text/javascript",
         },
-
+      ],
+      noscript: [
         {
-          textContent: `<!-- Google Tag Manager (noscript) -->
-<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}"
+          innerHTML: `<!-- Google Tag Manager (noscript) -->
+<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>
 <!-- End Google Tag Manager (noscript) -->`,
           tagPosition: "bodyOpen",
